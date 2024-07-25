f5f9d2db3edd61af14b1e3c449cec556de830b5b
LyonKabel <93550008+LyonKabel@users.noreply.github.com>
2024-07-25 15:50:50 -0500

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lyonkabel.github.io',
-    base: '/my-static-site'
+    base: '/my-static-site/'
 });
