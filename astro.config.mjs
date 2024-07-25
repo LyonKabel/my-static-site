74b027c7a273358a79be53b17ef21d06179ae909
LyonKabel <93550008+LyonKabel@users.noreply.github.com>
2024-07-25 16:27:15 -0500

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lyonkabel.github.io',
-    base: '/my-static-site/'
+  site: 'https://lyonkabel.github.io',
+  base: '/my-static-site/',
+  integrations: [tailwind()]
 });
